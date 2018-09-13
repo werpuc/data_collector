a926be0bb6140113dd22e0164b5cf57a28bd2324
werpuc <puchala.weronika@gmail.com>
2018-09-13 22:59:26 +0200

    manual data module + write_to_file

--- data_collector.py
+++ data_collector.py
@@ -18,6 +18,7 @@ def show_options():
     print('A : Manage preferences. Start here if you are a new user. ')
     print('B : Gather general data e.g. sleep time and mood. ')
     print('C : Start working. ')
+    print('D : Add work data manually. ')
     print('q : Quit program and do other things! ')
     print('Please choose one of the options when asked. ')
 
@@ -68,6 +69,46 @@ def manage_preferences():
         print('Work unit bundle: ' + str(get_preferences.get_work_unit_bundle()))
 
 
+def write_to_file(directory, file_name, *params):
+
+    """ Avoiding repetition """  
+
+    file_address = directory + '/' + file_name
+    text = ','.join(map(str, params)) 
+
+    try:
+        with open(file_address, "a+") as f:
+            f.write(text + '\n')
+    except IOError:
+        print('Data file manipulation failed!')
+        print(sys.exc_info()[0])
+
+
+def add_work_data_manually(directory, file_name):
+
+    """ Add data when worked outside of this tool """
+
+    print('So, I understand you worked with somebody else ;(')
+    is_today = yn_input_validator('Do you want to insert data only from today? [y/n] ')
+
+    if is_today:
+        while True:
+            try:
+                no_session = int(input('How many pomodoros have you worked today? '))
+                day = datetime.now().date()
+                write_to_file(directory, file_name, day, no_session)
+                break
+            except ValueError:
+                print('Please answer in numbers!')
+    else:
+        while True: 
+            day = input('When you worked? ')
+            no_session = input('How mych pomodoros? ')
+            write_to_file(directory, file_name, day, no_session)
+            is_end = yn_input_validator('Is this the end? [y/n] ')
+            if is_end: break
+
+
 def countdown(countdown_minutes, session_name):
 
     """ Function to display remaining time. Input wanted in minutes """
@@ -96,8 +137,7 @@ def working_body(directory, work_time_unit, short_break_time, long_break_time, w
 
     no_session = 0
     consent = yn_input_validator('Are you ready to work? [y/n] ')
-
-    f = open(directory+'/work_data_log.csv', 'a+')
+    file_name = 'work_data_log.csv'
 
     while consent:
 
@@ -106,22 +146,21 @@ def working_body(directory, work_time_unit, short_break_time, long_break_time, w
         work_start_at = get_nice_datetime()
         unit_type = 'work time unit'
         countdown(work_time_unit, 'Work time unit')
-        f.write(str(work_start_at) + ',' + unit_type + '\n')
+        write_to_file(directory, file_name, work_start_at, unit_type)
 
         if_break = yn_input_validator('Do you want to have a break? [y/n] ')
 
-        if_break:
+        if if_break:
             break_start_at = get_nice_datetime()
             break_time = long_break_time if no_session % work_unit_bundle == 0 else short_break_time
             unit_type = 'long break' if break_time > 20 else 'short break'
             print('Starting ' + unit_type)
             countdown(break_time, 'Break')
-            f.write(str(break_start_at) + ',' + unit_type + '\n')
+            write_to_file(directory, file_name, break_start_at, unit_type)
 
         print('Congratulations for finishing work unit! ')
         consent = yn_input_validator('Do you want to work more? [y/n] ')
 
-    f.close()
     print('That\'s it! Good work, everybody. ')
     return no_session
 
@@ -134,10 +173,11 @@ def main():
     show_options()
     action = input('Which one do you choose? ')
     directory = 'collected_data'
+    file_name = 'work_data.csv'
 
     if not os.path.exists(directory):
         os.makedirs(directory)
-
+   
     while action != 'q':
         
         if action in ('a', 'A'):
@@ -151,15 +191,10 @@ def main():
             long_break_time = get_preferences.get_long_break_time()
             work_unit_bundle = get_preferences.get_work_unit_bundle()
             no_session = working_body(directory, work_time_unit, short_break_time, long_break_time, work_unit_bundle)
+            write_to_file(directory, file_name, day, no_session)
 
-            try:
-                f = open(directory+'/work_data.csv', "a+")
-                f.write(str(day) + ',' + str(no_session) + '\n')
-                f.close()
-            except IOError:
-                print('Data file manipulation failed!')
-                print(sys.exc_info()[0])
-
+        elif action in ('d', 'D'):
+            add_work_data_manually(directory, file_name)
         else:
             print('Of course.')
             show_options()
